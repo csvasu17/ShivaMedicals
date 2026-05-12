562e630b052392389d186e6319e9a59eeeb52f97
vasu <csvasu17@gmail.com>
2026-05-13 01:04:54 +0530

    refactor code

--- server/update_types.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config({ path: './.env' });
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query('CREATE TABLE IF NOT EXISTS doctor_types (id SERIAL PRIMARY KEY, name VARCHAR UNIQUE NOT NULL);')
-  .then(() => pool.query(`INSERT INTO doctor_types (name) VALUES ('general'), ('child') ON CONFLICT DO NOTHING;`))
-  .then(() => console.log('Table created successfully'))
-  .catch(err => console.error(err))
-  .finally(() => pool.end());
